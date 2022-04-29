220b7a9631a0282ed75ad98a6c047480030a110e
tcui18 <tiffanyc.1262@gmail.com>
2022-04-29 21:29:19 +0000

    class done

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -19,7 +19,7 @@ class DirectorsController < ApplicationController
   def director_details
     the_id = params.fetch("id")
     @the_director = Director.where({:id => the_id})[0]
-
+    @filmography = Movie.where({:director_id => @the_director.id})
     render({:template => "directors_templates/show.html.erb"})
   end
 end
