404a95c1846e8d1cb5f35a8c890d1988b20f8e2c
tcui18 <tiffanyc.1262@gmail.com>
2022-04-29 21:20:37 +0000

    director stuff

---
+++ app/controllers/directors_controller.rb
@@ -0,0 +1,25 @@
+class DirectorsController < ApplicationController
+  def index
+    @list_of_directors = Director.all
+    render({:template => "directors_templates/index.html.erb"})
+  end
+
+  def eldest
+    @oldest = Director.where.not({:dob => nil})
+      .order({:dob => :asc})[0]
+    render({:template => "directors_templates/eldest.html.erb"})
+  end
+
+  def youngest
+    @youngest = Director.where.not({:dob => nil})
+    .order({:dob => :desc})[0]
+    render({:template => "directors_templates/youngest.html.erb"})
+  end
+
+  def director_details
+    the_id = params.fetch("id")
+    @the_director = Director.where({:id => the_id})[0]
+
+    render({:template => "directors_templates/show.html.erb"})
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   get("/", { :controller => "application", :action => "homepage" })
+  get("/directors", {:controller => "directors", :action => "index"})
+  get("directors/eldest", {:controller => "directors", :action => "eldest"})
+  get("directors/youngest", {:controller => "directors", :action => "youngest"})
+  get("directors/:id", {:controller => "directors", :action => "director_details"})
 end
